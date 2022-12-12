10abec8022accc38a6afc8aae0eecb6ebf61726c
Augusto Santana Guilherme <augustoosantgui@gmail.com>
2022-12-12 18:56:33 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Este é um formulário com validação nos inputs e um pouco de consumo de APIs feito com HTML, CSS e JavaScript.
+
+Neste projeto eu pratiquei um pouco de manipulação de DOM e aprendi sobre a API do browse: fecth, usando JavaScript.
---
+++ scripts.js
@@ -0,0 +1,85 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let data = {
+    name: username.value,
+    email: email.value,
+    pass: password.value,
+    passwordConfirmation: passwordConfirmation.value,
+  };
+  fetch("https://reqres.in/api/users", {
+    method: "post",
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log(data);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log("Deu tudo errado!");
+    });
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  const passwordConfirmationValue = passwordConfirmation.value;
+
+  if (usernameValue === "") {
+    setErrorFor(username, "O nome de usuário é obrigatório.");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "O email é obrigatório.");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "A senha é obrigatória.");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (passwordConfirmationValue === "") {
+    setErrorFor(passwordConfirmation, "A confirmação de senha é obrigatória.");
+  } else if (passwordConfirmationValue !== passwordValue) {
+    setErrorFor(passwordConfirmation, "As senhas não conferem.");
+  } else {
+    setSuccessFor(passwordConfirmation);
+  }
+}
+
+// Tratamento de erros
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
